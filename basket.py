de2c1021d9d45d54f1da6b6eaad6457cd3ae26e7
neel-guckool <neel001g@gmail.com>
2026-09-16 16:39:33 +0100

    basket

--- basket.py
+++ basket.py
@@ -18,3 +18,18 @@ print(f"Items only in basket 1: {only_basket_1}")
 
 only_basket_2 = basket_2.difference(basket_1)
 print(f"Items only in basket 2: {only_basket_2}")
+
+import array as arr
+items_prices = arr.array('f', [1.5, 2.0, 3.0, 4.5, 0.5])
+print("Prices of items in basket 1: ", items_prices)
+
+items_prices.insert(2, 3)
+items_prices.append(2.0)
+print("Updated prices of items in basket 1: ", items_prices)
+
+count_of_items = items_prices.count(2.0)
+print(f"Count of items with price 2.0: {count_of_items}")
+
+items_prices.reverse()
+print("Reversed prices of items in basket 1: ", items_prices)
+
